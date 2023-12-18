014fadd0c5899cabf46e00eebd50a55d75904189
Crenn Antoine <antoine.crenn@ynov.com>
2023-12-18 22:36:25 +0100

    ajout

--- src/main/kotlin/livremanagement/domaine/livre/Livre.kt
+++ src/main/kotlin/livremanagement/domaine/livre/Livre.kt
@@ -1,15 +1,3 @@
 package kotlin.livremanagement.domaine.livre
 
-data class Livre(val titre: String, val auteur: String, val reserver: Boolean = false) {
-    init {
-        verifiervide()
-    }
-    private fun verifiervide() {
-        if(titre.isNotBlank()) {
-            throw IllegalArgumentException("Titre vide")
-        }
-        if(auteur.isNotBlank()) {
-            throw IllegalArgumentException("Auteur vide")
-        }
-    }
-}
+data class Livre(val titre: String, val auteur: String, val reserver: Boolean = false)
